cad4dda08e7813eab0bceb8f1ba3509200fbd526
Gino Sanzi <eugene.sanzi@gmail.com>
2011-09-24 13:41:29 -0700

    Max_distance and hamming_distance_ltd finished
    HDL allows for a hamming_distance to be calculated even when
    the first test string is not as long as the second

--- lib/proj_math.rb
+++ lib/proj_math.rb
@@ -13,13 +13,27 @@ def hamming_distance(v, w)
   distance
 end
 
-def distance(v, dna)
-  return nil unless v.length == dna.line_size
+def hamming_distance_ltd(v, w)
+  v_arr = v.upcase.split("")
+  w_arr = w.upcase.split("")
+  w_arr = w_arr[0, v.length]
+  return nil unless v_arr.size == w_arr.size
+  
+  distance = 0
+  v_arr.each_index do |i|
+    if v_arr[i] != w_arr[i]
+      distance += 1
+    end
+  end
+  distance
+end
+
+def max_distance(v, dna)
   distance = 0
   c_dist = 0
   strands = dna.all_strands
-  strands.each_index do |i|
-    c_dist = hamming_distance v, strands[i]
+  strands.each do |strand|
+    c_dist = hamming_distance_ltd v, strand
     if c_dist > distance
       distance = c_dist
     end
@@ -27,11 +41,6 @@ def distance(v, dna)
   distance
 end
 
-def max_dist(score_a, score_c, score_g, score_t)
-  if score_a[0] > (score_c[0] and score_g[0] and score_t[0])
-    return score_a
-  end
-end
 
 
 
--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-require "./lib/dna"
-require "./lib/proj_math"
+$LOAD_PATH << './lib'
+require "dna"
+require "proj_math"
 
 def real_ans # for input 5
   "CAAATGCGTACTATCTCGTATCAAT"
@@ -23,11 +24,16 @@ end
 
 $current_worst = nil
 
-def calculate(dna, counter)
+def calculate(dna, string, counter)
   if counter == 0
     return Array.[](nil)
+  elsif 
+    calculate(dna, string + "A", counter - 1)
+    calculate(dna, string + "C", counter - 1)
+    calculate(dna, string + "G", counter - 1)
+    calculate(dna, string + "T", counter - 1)
   else
-    return append(calculate(dna, counter - 1))
+    return nil
   end
 end
 
@@ -35,8 +41,7 @@ filename = "./test_cases/" + (ARGV.first || "input1.txt")
 puts "Opening " + filename + "..."
 dna = DNA.new(filename)
 
-list = calculate dna, dna.line_length
-
+#list = calculate dna, dna.line_length
 
 
 
